a9acfadaff9d8ac3f547bc4fe43813310c72506b
ildar.bekniyazov <>
2023-10-14 21:48:28 +0600

    Commit 2. Generate questions and answers from the files

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,4 +1,5 @@
-﻿using Quiz;
+﻿using Newtonsoft.Json;
+using Quiz;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
@@ -8,9 +9,24 @@ using Telegram.Bot.Types.ReplyMarkups;
 
 public class Program
 {
-    public static int TestNumber;
+    public static int TestNumber = 0;
+    //public static List<string> strings = new() {"about", "afraid", "after" };
+    public static Dictionary<string, Dictionary<int, List<string>>> myDict = new Dictionary<string, Dictionary<int, List<string>>>(); 
+                  //{"about", new Dictionary<int, List<string>>{{1, new List<string>{"кричать", "бояться"}},
+                  //{"about", new Dictionary<int, List<string>>{{2, new List<string>{"кричать", "бояться"}} };
+    public static Dictionary<int, List<string>> fileList = new Dictionary<int, List<string>>();
+    public static Dictionary<string, KeyValuePair<int, string[]>> QADict = new Dictionary<string, KeyValuePair<int, string[]>>();
+
+
+
+
+    public static long chatId = 0;
     private static async Task Main(string[] args)
     {
+        var ft = new QAGenerator();
+        //Console.WriteLine("directory " + Directory.GetCurrentDirectory());
+        QADict = ft.Generate();
+        await Console.Out.WriteLineAsync(   $"returned dict cnt {QADict.Count}");
         var botClient = new TelegramBotClient("6390809244:AAEIdXZO3S3zDeDtiQp3_5tMYG_4VCwR7vw");
 
         using CancellationTokenSource cts = new();
@@ -29,7 +45,21 @@ public class Program
 
         var me = await botClient.GetMeAsync();
 
+        //fileList.Add(1, new List<string> { "о", "кольцо" });
+        //var ed = new Dictionary<int, List<string>> { { 2, new List<string> { "кричать", "бояться" } } };
+
+        myDict.Add("about", new Dictionary<int, List<string>> { { 0, new List<string> { "о", "кольцо" } } });
+        myDict.Add("afraid", new Dictionary<int, List<string>> { { 1, new List<string> { "кричать", "бояться" } } });
+
+        //fileList.Add(2, new List<string> { "кричать", "бояться" });
+        //fileList.Add(1, new List<string> { "vimal", "vilma" });
+
+        //myDict.Add("about", fileList);
+        //myDict.Add("afraid", fileList);
+
+
         Console.WriteLine($"Start listening for @{me.Username}");
+        Console.WriteLine($"me is {JsonConvert.SerializeObject(me)}");
         Console.ReadLine();
 
         cts.Cancel();
@@ -37,11 +67,14 @@ public class Program
         //update handler 
         async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
+            await Console.Out.WriteLineAsync($"Update type {update.Type.ToString()}");
             var handler = update.Type switch
             {
                 UpdateType.Message => HandleMessageAsync(botClient, update, cancellationToken),
                 UpdateType.CallbackQuery => HandleCallBackQueryAsync(botClient, update, cancellationToken),
-                
+                UpdateType.Poll => HandleMessageAsync(botClient, update, cancellationToken),
+                UpdateType.PollAnswer => HandleMessageAsync(botClient, update, cancellationToken),
+                _ => HandleMessageAsync(botClient, update, cancellationToken)
             };
 
             try
@@ -72,6 +105,8 @@ public class Program
     //CallBackQuery
     private static async Task HandleCallBackQueryAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
+        await Console.Out.WriteLineAsync("CallBackQuery");
+        await Console.Out.WriteLineAsync($"update.CallbackQuery is {JsonConvert.SerializeObject(update.CallbackQuery)}");
         var callBack = update.CallbackQuery;
 
         var tests = TestRepository.GetTests();
@@ -80,13 +115,14 @@ public class Program
         var nextTest = tests[TestNumber];
 
 
-        await CheckAnswerAsync(test,botClient, callBack, cancellationToken);
+        await CheckAnswerAsync(test, botClient, callBack, cancellationToken);
         TestNumber++;
         await SendNextQuestion(nextTest, botClient, update, cancellationToken);
     }
 
     private static async Task SendNextQuestion(Test nextTest, ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
+        Console.WriteLine("SendNextQuestion");
 
         InlineKeyboardMarkup inlineKeyboard = new(new[]
         {
@@ -94,30 +130,34 @@ public class Program
             new []
             {
                 InlineKeyboardButton.WithCallbackData(
-                    text: $"{nextTest.A}", 
+                    text: $"{nextTest.A}",
                     callbackData: "A"),
             },
             new []
             {
                 InlineKeyboardButton.WithCallbackData(
-                    text: $"{nextTest.B}", 
+                    text: $"{nextTest.B}",
                     callbackData: "B"),
             },
             new []
             {
                 InlineKeyboardButton.WithCallbackData(
-                    text: $"{nextTest.C}", 
+                    text: $"{nextTest.C}",
                     callbackData: "C"),
             },
             new []
             {
                 InlineKeyboardButton.WithCallbackData(
-                    text: $"{nextTest.D}", 
+                    text: $"{nextTest.D}",
                     callbackData: "D"),
             }
         });
 
-        if(update.Message is null )
+
+
+        await Console.Out.WriteLineAsync($"update.Message is {JsonConvert.SerializeObject(update.Message)}");
+
+        if (update.Message is null)
         {
             await botClient.SendTextMessageAsync(
                 chatId: update.CallbackQuery.From.Id,
@@ -139,7 +179,10 @@ public class Program
 
     private static async Task CheckAnswerAsync(Test test, ITelegramBotClient botClient, CallbackQuery? callBack, CancellationToken cancellationToken)
     {
-        if(callBack.Data == test.CorrectAnswer)
+        await Console.Out.WriteLineAsync("CheckAnswerAsync invoked");
+        await Console.Out.WriteLineAsync($"callBack.Data is {callBack.Data}");
+
+        if (callBack.Data == test.CorrectAnswer)
         {
             await botClient.SendTextMessageAsync(
                 chatId: callBack.From.Id,
@@ -159,24 +202,83 @@ public class Program
 
     private static async Task HandleMessageAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
-        if (update.Message is not { } message)
-            return;
-        if (message.Text is not { } messageText)
-            return;
-        
-        Console.WriteLine($"Received a '{messageText}' message in chat {update.Message.Chat.Id}.");
+        string? messageText = "";
+        await Console.Out.WriteLineAsync("HandleMessageAsync invoked");
+        await Console.Out.WriteLineAsync($"update.Type is {update.Type}");
 
-        if(messageText == "/start")
+        //if (update.Message is not { } message)
+        //    return;
+        //if (message.Text is not { } messageText)
+        //    return;
+
+        //Console.WriteLine($"Received a '{messageText}' message in chat {update.Message.Chat.Id}.");
+        //Console.WriteLine($"myDict[0] is {messageText}");
+        //if (update?.Message is not null)
+        //{ messageText = update.Message.Text; }
+
+        //var df = myDict.Take(TestNumber);
+        await Console.Out.WriteLineAsync($"QADict cnt is {QADict.Count}");
+
+        await Console.Out.WriteLineAsync($"QADict.ElementAt(TestNumber).Key is {QADict.ElementAt(TestNumber).Key}");
+
+
+        if (update.Type == UpdateType.Message)
         {
-            await botClient.SendTextMessageAsync(
-                chatId:update.Message.Chat.Id,
-                text:$"Welcome <b> {update.Message.From.FirstName} </b>",
-                parseMode: ParseMode.Html,
-                cancellationToken:cancellationToken);
+            chatId = update.Message.Chat.Id;
+            await botClient.SendPollAsync(
+                    chatId: chatId,
+                    question: QADict.ElementAt(TestNumber).Key,
+                    type: PollType.Quiz,
+                    correctOptionId: QADict.ElementAt(TestNumber).Value.Key,
+                    options: QADict.ElementAt(TestNumber).Value.Value,
+                    cancellationToken: cancellationToken);
+            TestNumber++;
+
+            //myDict.Take(TestNumber).Last().Key,
+            //myDict.Take(TestNumber).Last().Value.Keys.First(),
+            //myDict.Take(TestNumber).Last().Value.Values.First()
+
+            //await botClient.SendStickerAsync(
+            //     chatId: update.Message.Chat.Id,
+            //      sticker: InputFile.FromString("CAACAgIAAxkBAAOEZR-9wzhQFf_87F9ascUIiB_7oWEAAhUAA8A2TxPNVqY7YZ5k5zAE"),
+            //       cancellationToken: cancellationToken);
+
+
+            //        await botClient.SendPollAsync(
+            //chatId: update.Message.Chat.Id,
+            //question: "Did you ever hear the tragedy of Darth Plagueis The Wise?",
+            //type: PollType.Quiz,
+            //correctOptionId: 0,
+
+            //options: new[]
+            //{
+            //    "Yes for the hundredth time!",
+            //    "No, who`s that?",
+            //    "ddf fvxdfv?"
+            //},
+            //cancellationToken: cancellationToken);
+
+
+            //await botClient.SendTextMessageAsync(
+            //    chatId:update.Message.Chat.Id,
+            //    text:$"Welcome <b> {update.Message.From.FirstName} </b>",
+            //    parseMode: ParseMode.Html,
+            //    cancellationToken:cancellationToken);
+        }
+        if (update.Type == UpdateType.Poll)
+        {
+            await botClient.SendPollAsync(
+                    chatId: chatId,
+                    question: QADict.ElementAt(TestNumber).Key,
+                    type: PollType.Quiz,
+                    correctOptionId: QADict.ElementAt(TestNumber).Value.Key,
+                    options: QADict.ElementAt(TestNumber).Value.Value,
+                    cancellationToken: cancellationToken);
+            TestNumber++;
         }
 
-        TestNumber = 1;
+        //TestNumber = 1;
         var tests = TestRepository.GetTests();
-        await SendNextQuestion(tests[0],botClient,update,cancellationToken);
+        //await SendNextQuestion(tests[0],botClient,update,cancellationToken);
     }
 }
---
+++ Quiz/QAGenerator.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+
+namespace Quiz;
+
+public class QAGenerator
+{
+    public static Dictionary<string, KeyValuePair<int, string[]>> MyDict = new Dictionary<string, KeyValuePair<int, string[]>>();
+    public static List<KeyValuePair<int, string[]>> Dict1 = new();
+    public Dictionary<string, KeyValuePair<int, string[]>> Generate()
+    {
+        Random rnd = new Random();
+        //Generating questions and answers for telegram chat bot
+        //read list of questions from file
+        string currentDirectory = Directory.GetCurrentDirectory();
+        Console.WriteLine($"currentDirectory: {currentDirectory}");
+        string path1 = System.IO.Path.Combine(currentDirectory, "linesofqs.txt");
+        string path2 = System.IO.Path.Combine(currentDirectory, "linesofas.txt");
+        //string path2 = "C:\\Users\\User\\Documents\\linesofas.txt";
+
+        var lines1 = File.ReadLines(path1).ToArray();
+        var lines2 = File.ReadLines(path2).ToArray();
+
+        //Console.WriteLine(lines1.Count());
+        Console.WriteLine(lines2.Count());
+
+        //foreach (var line in lines1)
+        //{
+        //    //Console.WriteLine(line);
+        //    //myDict.Add(line, null);
+        //}
+        Console.WriteLine(Dict1.Count);
+
+        //foreach (var kvp in MyDict)
+        //{
+        //    Console.WriteLine(kvp.Key); 
+        //}
+
+        //foreach (var line in lines2)
+        //{
+        //    Console.WriteLine(line);
+        //    //MyDict.Add(line, null);
+        //}
+        var allIndexes = Enumerable.Range(0, 3).ToArray();
+        //Console.WriteLine($"allIndexes {JsonConvert.SerializeObject(allIndexes)}");
+        var assignedIndex = new int[1];
+
+        var linesindexes = Enumerable.Range(0, 2).ToArray();
+
+        //Console.WriteLine(allIndexes.Length);
+        //generate answers list
+        for (var i = 0; i < lines2.Length; i++)
+        {
+            string[] arr = new string[3];
+
+            var index1 = rnd.Next(3);
+            assignedIndex[0] = index1;
+            //Dict1.ElementAt(i);
+            var index2 = rnd.Next(lines2.Length);
+            var index3 = rnd.Next(lines2.Length);
+            while (index2 == index3)
+            {
+                index3 = rnd.Next(lines2.Length);
+            }
+            linesindexes[0] = index2;
+            linesindexes[1] = index3;
+            //Console.WriteLine($"linesindexes {JsonConvert.SerializeObject(linesindexes)}");
+
+            var freeindexes = allIndexes.Except(assignedIndex).ToArray();
+            //Console.WriteLine($"freeindexes {JsonConvert.SerializeObject(freeindexes)}");
+            arr[index1] = lines2[i];
+
+            for (var j = 0; j < freeindexes.Length; j++)
+            {
+                arr[freeindexes[j]] = lines2[linesindexes[j]];
+            }
+            //Console.WriteLine($"arr is {JsonConvert.SerializeObject(arr)}");
+
+            Dict1.Add(new KeyValuePair<int, string[]>(index1, arr));
+                                                                     
+        }
+
+
+        for (var i = 0; i < lines1.Length; i++)
+        {
+            MyDict.Add(lines1[i], Dict1[i]);
+        }
+
+        //foreach (var kvp in MyDict)
+        //{
+        //    Console.WriteLine(kvp.Key);
+        //    Console.WriteLine($"kvp.Value {JsonConvert.SerializeObject(kvp.Value)}");
+        //}
+
+        return MyDict;
+    }
+
+}
